9d92da8e0f5be8672453efc5f0bb0d3ca462f0e2
Lvzhou <coloz999@gmail.com>
2020-08-16 09:38:28 +0800

    更新

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'character-editor';
+
+  items = [];
+
+  charStr = ""
+
+  ngOnInit(): void {
+    for (let index = 0; index < 40; index++) {
+      this.items.push({ isSelected: false })
+    }
+    this.process()
+  }
+
+  selectBlock(item) {
+    console.log(item);
+    item.isSelected = !item.isSelected
+    this.process()
+  }
+
+  process() {
+    this.charStr = `byte newchar[8] = {\n\tB`
+    for (let index = 0; index < 40; index++) {
+      this.charStr += this.items[index].isSelected ? '1' : '0';
+      if ((index + 1) % 5 == 0 && (index + 1) != 40) this.charStr += `,\n\tB`
+    }
+    this.charStr +=`\n}`
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlockComponent } from './block/block.component';
+import { FormsModule }   from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
